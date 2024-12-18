9de2151d698933b6d52c9476a476d1a84d1cb1fb
Shashwat Singh <imshashwatsingh@gmai.com>
2024-12-18 12:35:36 +0530

    JavaScript Added !

---
+++ index.js
@@ -0,0 +1,5 @@
+// Get the current year
+const currentYear = new Date().getFullYear();
+
+// Add the text "Shashwat Singh" along with the current year
+document.getElementById("year").textContent = `${currentYear} © Shashwat Singh`;
